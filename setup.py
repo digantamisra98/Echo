b6118e547de86b82cdf0daba6ceecd8ff992ae15
Aleksandra Astakhova <lexie@live.ru>
2019-08-02 18:11:04 +0300

    Updated setup.py file: 1. Changed package veriosn to 1.0 2. Added authors' names and contacts 3. Added github url 4. Added packages list

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup
 
 setup(name='Echo',
-      version='0.1',
-      description='Python package containing all mathematical backend algorithms used in Machine Learning.',
-      packages=['Echo'],
+      version='1.0',
+      author='Diganta Misra, Aleksandra Deis',
+      author_email='mishradiganta91@gmail.com',
+      url='https://github.com/digantamisra98/Echo',
+      description='Python package containing SOTA mathematical backend algorithms used in Machine Learning. The initial release.',
+      packages=['Echo', 'Echo.Activation', 'Echo.Activation.Torch', 'Echo.Activation.Keras'],
       zip_safe=False)

bbeb28c12c553d728922082c50d12ece88d463ea
soumik12345 <19soumik.rakshit96@gmail.com>
2019-08-10 02:06:28 +0530

    Added ELiSH

--- Echo/Activation/Tensorflow/custom_activation.py
+++ Echo/Activation/Tensorflow/custom_activation.py
@@ -99,3 +99,14 @@ class BetaMish(Layer):
     
     def call(self, inputs):
         return inputs * tf.math.tanh(tf.math.log(tf.math.pow(1 + tf.math.exp(inputs), self.beta)))
+
+
+class ELiSH(Layer):
+
+    def __init__(self):
+        super(ELiSH, self).__init__()
+    
+    def call(self, inputs):
+        case_1 = tf.cast(tf.math.greater_equal(inputs, 0), 'float32') * inputs * tf.math.sigmoid(inputs)
+        case_2 = tf.cast(tf.math.less(inputs, 0), 'float32') * (tf.math.exp(inputs) - 1) * tf.math.sigmoid(inputs)
+        return case_1 + case_2

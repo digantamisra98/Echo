385be87d60e1625b77b54f2f00fc78c88ac5418d
Aleksandra Astakhova <lexie@live.ru>
2019-08-11 08:53:24 +0300

    Fixed unit tests after renaming to camel case.

--- Unit_tests/torch_activation_tests.py
+++ Unit_tests/torch_activation_tests.py
@@ -15,24 +15,24 @@ import torch
 from torch import nn
 
 # import custom activations from Echo
-from Echo.Activation.Torch.weightedTanh import weightedTanh
-from Echo.Activation.Torch.mish import mish
-from Echo.Activation.Torch.silu import silu
-from Echo.Activation.Torch.aria2 import aria2
-from Echo.Activation.Torch.eswish import eswish
-from Echo.Activation.Torch.swish import swish
-from Echo.Activation.Torch.beta_mish import beta_mish
-from Echo.Activation.Torch.elish import elish
-from Echo.Activation.Torch.hard_elish import hard_elish
-from Echo.Activation.Torch.mila import mila
-from Echo.Activation.Torch.sine_relu import sine_relu
-from Echo.Activation.Torch.fts import fts
-from Echo.Activation.Torch.sqnl import sqnl
-from Echo.Activation.Torch.isru import isru
-from Echo.Activation.Torch.isrlu import isrlu
-from Echo.Activation.Torch.bent_id import bent_id
-from Echo.Activation.Torch.soft_clipping import soft_clipping
-import Echo.Activation.Torch.functional as Func
+from echoAI.Activation.Torch.weightedTanh import WeightedTanh
+from echoAI.Activation.Torch.mish import Mish
+from echoAI.Activation.Torch.silu import Silu
+from echoAI.Activation.Torch.aria2 import Aria2
+from echoAI.Activation.Torch.eswish import Eswish
+from echoAI.Activation.Torch.swish import Swish
+from echoAI.Activation.Torch.beta_mish import BetaMish
+from echoAI.Activation.Torch.elish import Elish
+from echoAI.Activation.Torch.hard_elish import HardElish
+from echoAI.Activation.Torch.mila import Mila
+from echoAI.Activation.Torch.sine_relu import SineReLU
+from echoAI.Activation.Torch.fts import FTS
+from echoAI.Activation.Torch.sqnl import SQNL
+from echoAI.Activation.Torch.isru import ISRU
+from echoAI.Activation.Torch.isrlu import ISRLU
+from echoAI.Activation.Torch.bent_id import BentID
+from echoAI.Activation.Torch.soft_clipping import SoftClipping
+import echoAI.Activation.Torch.functional as Func
 
 # class containing unit tests for PyTorch activation functions
 class TestTorchActivations(TestCase):
@@ -43,9 +43,9 @@ class TestTorchActivations(TestCase):
     def test_weightedTanh_1(self):
         '''
         Unit test for weighted tanh activation function.
-        See :mod:`Echo.Activation.Torch.weightedTanh`.
+        See :mod:`Echo.Activation.Torch.WeightedTanh`.
         '''
-        weighted_tanh = weightedTanh(weight = 1)
+        weighted_tanh = WeightedTanh(weight = 1)
         input = torch.tensor((2.0,2.0))
         # checking that weighted tahn with weight equal to 1 is equal to pytorch weighted tanh
         self.assertEqual(torch.all(torch.eq(weighted_tanh(input), torch.tanh(input))) , True)
@@ -53,9 +53,9 @@ class TestTorchActivations(TestCase):
     def test_weightedTanh_2(self):
         '''
         Unit test for weighted tanh activation function.
-        See :mod:`Echo.Activation.Torch.weightedTanh`.
+        See :mod:`Echo.Activation.Torch.WeightedTanh`.
         '''
-        weighted_tanh = weightedTanh(weight = 5.0)
+        weighted_tanh = WeightedTanh(weight = 5.0)
         input = torch.tensor((0.0,0.0))
         output = torch.tensor((0.0,0.0))
         # checking that weighted tahn of 0 is 0
@@ -64,9 +64,9 @@ class TestTorchActivations(TestCase):
     def test_weightedTanh_3(self):
         '''
         Unit test for weighted tanh activation function.
-        See :mod:`Echo.Activation.Torch.weightedTanh`.
+        See :mod:`Echo.Activation.Torch.WeightedTanh`.
         '''
-        weighted_tanh = weightedTanh(weight = 2.0)
+        weighted_tanh = WeightedTanh(weight = 2.0)
         input = torch.tensor((1.1,1.1))
         output = torch.tensor((0.975743,0.975743))
         # checking that weighted tahn of 2.2 is 0.975743
@@ -75,9 +75,9 @@ class TestTorchActivations(TestCase):
     def test_weightedTanh_4(self):
         '''
         Unit test for weighted tanh activation function.
-        See :mod:`Echo.Activation.Torch.weightedTanh`.
+        See :mod:`Echo.Activation.Torch.WeightedTanh`.
         '''
-        weighted_tanh = weightedTanh(weight = 2.0, inplace = True)
+        weighted_tanh = WeightedTanh(weight = 2.0, inplace = True)
         input = torch.tensor((1.1,1.1))
         output = torch.tensor((0.975743,0.975743))
         # check the inplace implementation
@@ -88,9 +88,9 @@ class TestTorchActivations(TestCase):
     def test_silu_1(self):
         '''
         Unit test for SiLU activation function.
-        See :mod:`Echo.Activation.Torch.silu`.
+        See :mod:`Echo.Activation.Torch.Silu`.
         '''
-        fsilu = silu()
+        fsilu = Silu()
         input = torch.tensor((0.0,0.0))
         output = torch.tensor((0.0,0.0))
         # checking that silu(0) == 0
@@ -99,9 +99,9 @@ class TestTorchActivations(TestCase):
     def test_silu_2(self):
         '''
         Unit test for SiLU activation function.
-        See :mod:`Echo.Activation.Torch.silu`.
+        See :mod:`Echo.Activation.Torch.Silu`.
         '''
-        fsilu = silu()
+        fsilu = Silu()
         input = torch.tensor((1.0,1.0))
         output = torch.tensor((0.731058,0.731058))
 
@@ -111,10 +111,10 @@ class TestTorchActivations(TestCase):
     def test_silu_3(self):
         '''
         Unit test for SiLU activation function.
-        See :mod:`Echo.Activation.Torch.silu`.
+        See :mod:`Echo.Activation.Torch.Silu`.
         '''
         # checking an inplace implementation
-        fsilu = silu(inplace = True)
+        fsilu = Silu(inplace = True)
         input = torch.tensor((1.0,1.0))
         output = torch.tensor((0.731058,0.731058))
 
@@ -127,10 +127,10 @@ class TestTorchActivations(TestCase):
     def test_mish_1(self):
         '''
         Unit test for Mish activation function.
-        See :mod:`Echo.Activation.Torch.mish`.
+        See :mod:`Echo.Activation.Torch.Mish`.
         '''
         # checking that mish(0) == 0
-        fmish = mish()
+        fmish = Mish()
         input = torch.tensor((0.0,0.0))
         output = torch.tensor((0.0,0.0))
 
@@ -139,10 +139,10 @@ class TestTorchActivations(TestCase):
     def test_mish_2(self):
         '''
         Unit test for Mish activation function.
-        See :mod:`Echo.Activation.Torch.mish`.
+        See :mod:`Echo.Activation.Torch.Mish`.
         '''
         # checking that mish(1) == 0.865098
-        fmish = mish()
+        fmish = Mish()
         input = torch.tensor((1.0,1.0))
         output = torch.tensor((0.865098,0.865098))
 
@@ -151,11 +151,11 @@ class TestTorchActivations(TestCase):
     def test_mish_3(self):
         '''
         Unit test for Mish activation function.
-        See :mod:`Echo.Activation.Torch.mish`.
+        See :mod:`Echo.Activation.Torch.Mish`.
         '''
         # checking the in-place implementation of mish
         # checking that mish(1) == 0.865098
-        fmish = mish(inplace = True)
+        fmish = Mish(inplace = True)
         input = torch.tensor((1.0,1.0))
         output = torch.tensor((0.865098,0.865098))
         fmish(input)
@@ -165,11 +165,11 @@ class TestTorchActivations(TestCase):
     def test_aria2_1(self):
         '''
         Unit test for Aria2 activation function.
-        See :mod:`Echo.Activation.Torch.aria2`.
+        See :mod:`Echo.Activation.Torch.Aria2`.
         '''
         # checking that aria2(0, 0) = (0.5, 0.5)
         input = torch.tensor((.0,.0))
-        aria = aria2(beta=1., alpha=1.)
+        aria = Aria2(beta=1., alpha=1.)
         output = aria(input)
 
         self.assertEqual(output.allclose(torch.tensor((.5,.5))), True)
@@ -177,11 +177,11 @@ class TestTorchActivations(TestCase):
     def test_aria2_2(self):
         '''
         Unit test for Aria2 activation function.
-        See :mod:`Echo.Activation.Torch.aria2`.
+        See :mod:`Echo.Activation.Torch.Aria2`.
         '''
         # checking that aria2(1., 1.) = (0.73105, 0.73105)
         input = torch.tensor((1.0,1.0))
-        aria = aria2(beta=1., alpha=1.)
+        aria = Aria2(beta=1., alpha=1.)
         output = aria(input)
 
         self.assertEqual(output.allclose(torch.tensor((0.7310585786,0.7310585786))), True)
@@ -189,11 +189,11 @@ class TestTorchActivations(TestCase):
     def test_beta_mish_1(self):
         '''
         Unit test for Beta Mish activation function.
-        See :mod:`Echo.Activation.Torch.beta_mish`.
+        See :mod:`Echo.Activation.Torch.BetaMish`.
         '''
         # checking that beta_mish(0., 0.) = (0. , 0.)
         input = torch.tensor((0.0,0.0))
-        beta_mish_activation = beta_mish(beta=1.)
+        beta_mish_activation = BetaMish(beta=1.)
         output = beta_mish_activation(input)
 
         self.assertEqual(output.allclose(torch.tensor((0.0,0.0))), True)
@@ -201,11 +201,11 @@ class TestTorchActivations(TestCase):
     def test_beta_mish_2(self):
         '''
         Unit test for Beta Mish activation function.
-        See :mod:`Echo.Activation.Torch.beta_mish`.
+        See :mod:`Echo.Activation.Torch.BetaMish`.
         '''
         # checking that beta_mish(1., 1.) = (0.865098, 0.865098) beta = 1.0
         input = torch.tensor((1.0,1.0))
-        beta_mish_activation = beta_mish(beta=1.)
+        beta_mish_activation = BetaMish(beta=1.)
         output = beta_mish_activation(input)
 
         self.assertEqual(output.allclose(torch.tensor((0.865098,0.865098))), True)

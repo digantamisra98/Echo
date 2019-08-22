47a0f97fc63e9b8c2771ae2e83e386472a8fd8ca
soumik12345 <19soumik.rakshit96@gmail.com>
2019-08-22 17:35:48 +0530

    Changed Implementation of Mish

--- echoAI/Activation/TF_Keras/custom_activation.py
+++ echoAI/Activation/TF_Keras/custom_activation.py
@@ -332,7 +332,7 @@ class Mish(Layer):
         super(Mish, self).__init__()
 
     def call(self, inputs):
-        return inputs * tf.math.tanh(tf.math.log(1 + tf.math.exp(inputs)))
+        return inputs * tf.math.tanh(tf.math.softplus(inputs))
 
 
 class BetaMish(Layer):

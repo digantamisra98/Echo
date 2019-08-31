6f4c546b6dc3dd90e56ee802fb46cae6654ddc1a
soumik12345 <19soumik.rakshit96@gmail.com>
2019-08-31 19:59:05 +0530

    Added Gated Conv Block

--- Echo/Activation/Keras/custom_activations.py
+++ Echo/Activation/Keras/custom_activations.py
@@ -7,6 +7,8 @@ from __future__ import print_function
 
 from keras.engine.base_layer import Layer
 from keras import backend as K
+from keras import activations
+from keras.layers import Wrapper, Lambda
 from keras import initializers
 
 class mila(Layer):
@@ -1234,3 +1236,150 @@ class srelu(Layer):
 
     def compute_output_shape(self, input_shape):
         return input_shape
+
+
+class BReLU(Layer):
+
+    def __init__(self):
+        super(BReLU, self).__init__()
+    
+    def call(self, inputs):
+        def brelu(x):
+            #get shape of X, we are interested in the last axis, which is constant
+            shape = K.int_shape(x)
+            #last axis
+            dim = shape[-1]
+            #half of the last axis (+1 if necessary)
+            dim2 = dim // 2
+            if dim % 2 != 0:
+                dim2 += 1
+            #multiplier will be a tensor of alternated +1 and -1
+            multiplier = K.ones((dim2,))
+            multiplier = K.stack([multiplier, -multiplier], axis = -1)
+            if dim % 2 != 0:
+                multiplier = multiplier[:-1]
+            #adjust multiplier shape to the shape of x
+            multiplier = K.reshape(multiplier, tuple(1 for _ in shape[:-1]) + (-1,))
+            return multiplier * tf.nn.relu(multiplier * x)
+        return Lambda(brelu)(inputs)
+
+
+class GatedConvBlock(Wrapper):
+    
+    def __init__(self, conv_layer, conv_num = 3, gate_activation='sigmoid', **kwargs):
+        super(GatedConvBlock, self).__init__(conv_layer,**kwargs)
+        self.conv_num= conv_num
+        self.gate_activation = activations.get(gate_activation)
+        self.conv_layers = []
+        self.input_spec = conv_layer.input_spec 
+        self.rank = conv_layer.rank
+        if conv_layer.padding != 'same':
+            raise ValueError("The padding mode of this layer must be `same`, But found `{}`".format(self.padding))
+        self.filters = conv_layer.filters//2
+        #create conv layers 
+        import copy
+        for i in range(self.conv_num):
+            new_conv_layer = copy.deepcopy(conv_layer)
+            new_conv_layer.name = 'GatedConvBlock_{}_{}'.format(conv_layer.name, i)
+            self.conv_layers.append(new_conv_layer) 
+    
+    def build(self, input_shape):
+        if self.conv_layers[0].filters != input_shape[-1]*2:
+            raise ValueError("For efficient, the sub-conv-layer's filters must be the twice of input_shape[-1].\nBut found filters={},input_shape[-1]={}".format(self.conv_layers[0].filters, input_shape[-1]))
+        input_shape_current = input_shape
+        for layer in self.conv_layers:
+            with K.name_scope(layer.name):
+                layer.build(input_shape_current)
+            input_shape_current = input_shape
+        self.built = True            
+        pass
+    
+    def compute_output_shape(self, input_shape):
+        input_shape_current = input_shape
+        for layer in self.conv_layers:
+            input_shape_current = layer.compute_output_shape(input_shape_current)
+            output_shape = list(input_shape_current)
+            output_shape[-1] = int(output_shape[-1]/2)
+            input_shape_current = output_shape   
+        return tuple(input_shape_current)
+    
+    def half_slice(self, x):
+        ndim = self.rank +2
+        if ndim ==3:
+            linear_output = x[:,:,:self.filters]
+            gated_output = x[:,:,self.filters:]
+        elif ndim ==4:
+            linear_output = x[:,:,:,:self.filters]
+            gated_output = x[:,:,:,self.filters:]
+        elif ndim ==5:
+            linear_output = x[:,:,:,:,:self.filters]
+            gated_output = x[:,:,:,:,self.filters:]
+        else:
+            raise ValueError("This class only support for 1D, 2D, 3D conv, but the input's ndim={}".format(ndim))
+        return linear_output, gated_output
+    
+    def call(self, inputs):
+        input_current = inputs  
+        for i,layer in enumerate(self.conv_layers):
+            output_current = layer(inputs= input_current) 
+            linear_output, gated_output = self.half_slice(output_current)
+            input_current = linear_output*self.gate_activation(gated_output)
+            input_current._keras_shape = K.int_shape(linear_output)
+        #residual connection
+        output = input_current + inputs
+        return output
+    
+    def get_weights(self):
+        weights = None 
+        for layer in self.conv_layers:
+            weights += layer.get_weights()
+        return weights
+    
+    def set_weights(self, weights):
+        for layer in self.conv_layers:
+            layer.set_weights(weights)
+        pass
+    
+    @property
+    def trainable_weights(self):
+        weights = []
+        for layer in self.conv_layers:
+            if hasattr(layer, 'trainable_weights'):
+                weights += layer.trainable_weights
+        return weights
+        pass
+    
+    @property
+    def non_trainable_weights(self):
+        weights = []
+        for layer in self.conv_layers:
+            if hasattr(layer, 'non_trainable_weights'):
+                weights += layer.non_trainable_weights
+        return weights
+        pass
+    
+    @property
+    def updates(self):
+        updates_ = []
+        for layer in self.conv_layers:
+            if hasattr(layer, 'updates'):
+                updates_ += layer.upates
+        return updates_
+        pass
+    
+    @property
+    def losses(self):
+        losses_ = []
+        for layer in self.conv_layers:
+            if hasattr(layer, 'losses'):
+                losses_ += layer.losses
+        return losses_
+        pass
+    
+    @property
+    def constraints(self):
+        constraints_ = {}
+        for layer in self.conv_layers:
+            if hasattr(layer, 'constraints'):
+                constraints_.update(layer.constraints)
+        return constraints_

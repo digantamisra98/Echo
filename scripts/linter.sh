c9ef1eef12590cc0edae08ca4f8aee76b21ec431
digantamisra98 <mishradiganta91@gmail.com>
2020-11-19 11:23:54 +0530

    Updated Black and Flake8

--- scripts/linter.sh
+++ scripts/linter.sh
@@ -1,4 +1,3 @@
 # Checks the black formatting and runs Flake8 linter over python code
 
-python ./scripts/verify.py --checks black
-python ./scripts/verify.py --checks flake8
+python ./scripts/verify.py --checks black flake8

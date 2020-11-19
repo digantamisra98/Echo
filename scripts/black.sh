329f64aee766a42b0ab55a28202e0f4f05c17047
digantamisra98 <mishradiganta91@gmail.com>
2020-11-19 11:05:19 +0530

    updated precommit

--- scripts/black.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Checks the black formatting
-
-python ./scripts/verify.py --checks black
---
+++ scripts/linter.sh
@@ -0,0 +1,4 @@
+# Checks the black formatting and runs Flake8 linter over python code
+
+python ./scripts/verify.py --checks black
+python ./scripts/verify.py --checks flake8

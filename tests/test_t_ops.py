af8c4395014b43a8f9b71630257c2180f6d59660
iyaja <ajayuppili@gmail.com>
2020-11-26 01:22:12 +0530

    feat: move test suite to deeptest

--- tests/test_t_ops.py
+++ tests/test_t_ops.py
@@ -4,64 +4,31 @@ import torch
 from parameterized import parameterized
 
 from echoAI.Activation.t_ops import *
+from deeptest.functional import *
 
-TEST_SWISH = [
-    Swish(swish=True),
+TEST_SILU = [
+    Swish(swish=False),
+    'x * sigmoid(x)',
     torch.tensor([[[[-10, -8, -6, -4, -2], [0, 2, 4, 6, 8]]]], dtype=torch.float32),
-    torch.tensor(
-        [
-            [
-                [
-                    [-4.54e-04, -2.68e-03, -1.48e-02, -7.19e-02, -2.38e-01],
-                    [0.00e00, 1.76e00, 3.93e00, 5.99e00, 8.00e00],
-                ]
-            ]
-        ]
-    ),
     (1, 1, 2, 5),
 ]
 
 TEST_MISH = [
     Mish(),
+    'x * tanh(log(1 + e^x))',
     torch.tensor([[[[-10, -8, -6, -4, -2], [0, 2, 4, 6, 8]]]], dtype=torch.float32),
-    torch.tensor(
-        [
-            [
-                [
-                    [-4.54e-04, -2.68e-03, -1.49e-02, -7.26e-02, -2.53e-01],
-                    [0.00e00, 1.94e00, 4.00e00, 6.00e00, 8.00e00],
-                ]
-            ]
-        ]
-    ),
     (1, 1, 2, 5),
 ]
 
-TEST_BRELU = [
-    BReLU(),
-    torch.tensor([[[[-10, -8, -6, -4, -2], [0, 2, 4, 6, 8]]]], dtype=torch.float32),
-    torch.tensor(
-        [
-            [
-                [
-                    [-4.54e-04, -2.68e-03, -1.49e-02, -7.26e-02, -2.53e-01],
-                    [0.00e00, 1.94e00, 4.00e00, 6.00e00, 8.00e00],
-                ]
-            ]
-        ]
-    ),
-    (1, 1, 2, 5),
-]
 
 
 class TestActivations(unittest.TestCase):
-    @parameterized.expand([TEST_SWISH, TEST_MISH, TEST_BRELU])
-    def test_activations_value_shape(self, cls, inp, out, exp):
-        result = cls(inp)
-        torch.testing.assert_allclose(result, out, rtol=1e-2, atol=1e-5)
-        self.assertTupleEqual(result.shape, exp)
+
+    @parameterized.expand([TEST_SILU, TEST_MISH])
+    def test_activations_value_shape(self, fn, fn_expr, xs, shape):
+        tester.test(fn, fn_expr, xs, shape)
 
 
 if __name__ == "__main__":
-
+    tester = WolframTester('QYU645-4EGHX3JVLE', 'torch')
     unittest.main()
